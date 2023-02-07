242f8d1d1878323829cb6cfa7ad33ca3b71de700
boobaGreen <devboobagreen@gmail.com>
2023-02-07 18:44:54 +0100

    linkprogettiok

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,10 @@
 function myClick() {
   document.getElementById("thema-selection").classList.toggle("click-active");
   document.getElementById("body").classList.toggle("click-active");
+
+  /* Cambio contenuto TESTO PRINCIPALE*/
+  // const main = document.getElementById("principal-space");
+  // const text1 = `tic grizzled apprentice <a href="mailto:claudiodallara77@gmail.com">Claudio&nbsp;Dall'Ara</a> tic grizzled apprentice`;
+  // shows the alert
+  // main.innerHTML = text1;
 }

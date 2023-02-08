8b966451a5be6c105a583b3582875b79897a4b0c
boobaGreen <devboobagreen@gmail.com>
2023-02-08 14:05:06 +0100

    cambio_frasi_ver1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,44 @@
+text_main = `Hi, I'm
+<a href="https://boobagreen.github.io/portfolio/"
+          >Claudio&nbsp;Dall'Ara</a
+        >, a curious and enthusiastic grizzled apprentice
+        <a href="#/" id="code">coder</a>. I too have
+        <a href="#/" id="passion">passions</a> and
+        <a href="#/" id="project">projects</a> ...
+`;
+text_code = `
+Hi, I'm
+<a href="https://boobagreen.github.io/portfolio/">Claudio&nbsp;Dall'Ara</a>,
+some of my projects of the last period are in <a href="https://boobagreen.github.io/html_exercise/" target="_blank"> Html </a>, advanced <a href="https://boobagreen.github.io/advancedscss_claudio/" target="_blank"> CSS/SASS </a> and 
+<a href="https://forkify-claudiodallara77.netlify.app/" target="_blank">Javascript</a>.
+`;
+
 function myClick() {
   document.getElementById("thema-selection").classList.toggle("click-active");
   document.getElementById("body").classList.toggle("click-active");
-
-  /* Cambio contenuto TESTO PRINCIPALE*/
-  // const main = document.getElementById("principal-space");
-  // const text1 = `tic grizzled apprentice <a href="mailto:claudiodallara77@gmail.com">Claudio&nbsp;Dall'Ara</a> tic grizzled apprentice`;
-  // shows the alert
-  // main.innerHTML = text1;
 }
+
+function check_coder() {
+  var myDiv = document.getElementById("principal-space");
+  myDiv.innerHTML = text_code;
+}
+
+function callback(e) {
+  var e = window.e || e;
+  console.log(e);
+  console.log(e.target);
+  console.log(e.target.tagName);
+  if (e.target.tagName !== "A") return;
+
+  check_coder();
+}
+
+const init = function () {
+  if (document.addEventListener)
+    document.addEventListener("click", callback, false);
+  else document.attachEvent("onclick", callback);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  init();
+});

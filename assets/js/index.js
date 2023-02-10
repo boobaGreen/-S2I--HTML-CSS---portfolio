baca5b5bd064b3eead8825ec1ceb3d12da17ca11
boobaGreen <devboobagreen@gmail.com>
2023-02-10 02:21:33 +0100

    back

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,5 @@
 text_main = `
-Hi, I'm
-        <a href="#/" id="main" rel="noopener">Claudio&nbsp;Dall'Ara</a>, a
+Hi, I'm Claudio Dall'Ara, a
         <a href="#/" id="curious" rel="noopener">curious</a> and
         <a href="#/" id="project" rel="noopener">enthusiastic</a> apprentice
         <a href="#/" id="code" rel="noopener">coder</a>.
@@ -10,8 +9,7 @@ Hi, I'm
         <br />&darr; Let's work together
 `;
 text_curious = `
-Hi, I'm
-<a href="#/" id="main">Claudio&nbsp;Dall'Ara</a>,
+Hi, I'm Claudio Dall'Ara,
 I often get lost in <a href="https://www.youtube.com/watch?v=kemivUKb4f4" target="_blank"">music</a>,
 <a href="https://www.youtube.com/watch?v=8AVqNtDYAY0" target="_blank"">retro-gaming</a>,
  watching <a href="https://www.youtube.com/watch?v=SlDvRWuDmRk" target="_blank"">Serie C</a> and TV 
@@ -20,27 +18,23 @@ I often get lost in <a href="https://www.youtube.com/watch?v=kemivUKb4f4" target
  <br />&darr; Let's work together
 `;
 text_project = `
-Hi, I'm
-<a href="#/" id="main">Claudio&nbsp;Dall'Ara</a>,
+Hi, I'm Claudio Dall'Ara,
  currently engaged in a <a href="https://www.start2impact.it/master/full-stack-development/" target="_blank">full stack development</a> course. Active in various<a href="https://github.com/boobaGreen/dydx" target="_blank"> Python </a>AWS DeFi bot <a href="https://www.udemy.com/user/shaun34/" target="_blank"> projects</a>.
  <br />&darr; Let's work together
 `;
 text_code = `
-Hi, I'm
-<a href="#/" id="main">Claudio&nbsp;Dall'Ara</a>,
+Hi, I'm Claudio Dall'Ara,
 some of my projects of the last period are in <a href="https://boobagreen.github.io/html_exercise/" target="_blank"> Html</a>, advanced <a href="https://boobagreen.github.io/advancedscss_claudio/" target="_blank"> CSS/SASS </a> and 
 <a href="https://forkify-claudiodallara77.netlify.app/" target="_blank">Javascript</a>.
 <br />&darr; Let's work together
 `;
 text_work = `
-Hi, I'm
-<a href="#/" id="main">Claudio&nbsp;Dall'Ara</a>,
+Hi, I'm Claudio Dall'Ara,
  last long job as <a href="https://www.unieuro.it/online/" target="_blank">Unieuro</a> store manager, current at <a href="https://www.unieurogambettola.it/" target="_blank">Maroni Group</a>.
  <br />&darr; Let's work together
 `;
 text_interest = `
-Hi, I'm
-<a href="#/" id="main">Claudio&nbsp;Dall'Ara</a>,
+Hi, I'm Claudio Dall'Ara,
 sometimes I find myself delving into something about 
 <a href="https://www.youtube.com/watch?v=bB9F6u5D178" target="_blank">guitar</a>,
 stuff regarding <a href="https://www.youtube.com/watch?v=-MDgxwhigSQ&t=3541s" target="_blank">design</a>,
@@ -50,8 +44,7 @@ stuff regarding <a href="https://www.youtube.com/watch?v=-MDgxwhigSQ&t=3541s" ta
 <br />&darr; Let's work together
 `;
 text_dream = `
-Hi, I'm
-<a href="#/" id="main">Claudio&nbsp;Dall'Ara</a>, my dream jobs are <a href="https://www.raiplay.it/programmi/adrianoolivetti" target="_blank">Olivetti </a>in the 60s,
+Hi, I'm Claudio Dall'Ara, my dream jobs are <a href="https://www.raiplay.it/programmi/adrianoolivetti" target="_blank">Olivetti </a>in the 60s,
 <a href="https://en.wikipedia.org/wiki/Jeff_Minter" target="_blank">Llamasoft</a> in the 80s,
 <a href="https://www.binance.com/it" target="_blank">Binance</a> now.
 <br />&darr; Let's work together
@@ -65,17 +58,19 @@ function myClick() {
 
 function change_main(name_link) {
   var myDiv = document.getElementById("principal-space");
-  console.log(name_link);
   if (name_link !== "") {
-    myDiv.innerHTML = window["text_" + name_link];
+    if (name_link == "back") {
+      myDiv.innerHTML = text_main;
+      document.getElementById("footer-dx").classList.toggle("main-page");
+    } else {
+      myDiv.innerHTML = window["text_" + name_link];
+      document.getElementById("footer-dx").classList.toggle("main-page");
+    }
   }
 }
 
 function callback(e) {
   var e = window.e || e;
-  console.log(e);
-  console.log(e.target);
-  console.log(e.target.tagName);
   if (e.target.tagName !== "A") return;
   change_main(e.target.id);
 }
